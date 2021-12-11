353689368aae0d6cc2cf0d6ee1908ee32e305329
Margo1403 <margos14031977@gmail.com>
2021-12-11 13:08:45 +0200

    дополнегия

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$(".button-transparent open-popup").click(function(){
+$(".button-transparent.open-popup").click(function(){
     $(".modal-wrapper").fadeIn(600);
-}
+})
 $(".close-feedback").click(function(){
     $(".modal-wrapper").fadeOut(600);
-}
+})
