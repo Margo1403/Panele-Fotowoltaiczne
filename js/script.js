47c06a00c781c8a35e0410b7f637a1a2ac6dc21b
Margo1403 <margos14031977@gmail.com>
2021-12-15 13:22:35 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-$(".button-transparent.open-popup").click(function(){
-    $(".modal-wrapper").fadeIn(600);
+const callButton = document.querySelector(".button-transparent.open-popup")
+const callPopup = document.querySelector(".modal-wrapper")
+const callPopupCloseButton = callPopup.querySelector(".close-feedback-white") 
+
+callButton.addEventListener("click", function(){
+    callPopup.classList.remove("hide")
 })
-$(".close-feedback").click(function(){
-    $(".modal-wrapper").fadeOut(600);
+
+callPopupCloseButton.addEventListener("click", function(){
+    callPopup.classList.add("hide")
 })
+
+
+
+
+
+
+const callButton = document.querySelector(".welcome-orange-button.open-popup")
+const callPopup = document.querySelector(".modal-cection")
+
+
+const callButton = document.querySelector(".price.open-popup")
+const callPopup = document.querySelector(".second hide")
+
+
+const callButton = document.querySelector(".orange-inguiry.open-popup")
+const callPopup = document.querySelector(".orange hide")
