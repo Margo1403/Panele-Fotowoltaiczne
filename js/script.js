27b64b98e1a1f04bc68b8cd8e5ecf073fe29398d
Margo1403 <margos14031977@gmail.com>
2021-12-19 18:26:04 +0200

    сохранить

--- js/script.js
+++ js/script.js
@@ -15,13 +15,49 @@ callPopupCloseButton.addEventListener("click", function(){
 
 
 
-const callButton = document.querySelector(".welcome-orange-button.open-popup")
-const callPopup = document.querySelector(".modal-cection")
+const countButton = document.querySelector(".welcome-orange-button")
+const countPopup = document.querySelector(".modal-cection")
+const countPopupCloseButton = countPopup.querySelector(".close-feedback-white") 
+
+countButton.addEventListener("click", function(){
+    countPopup.classList.remove("hide")
+})
+
+countPopupCloseButton.addEventListener("click", function(){
+    countPopup.classList.add("hide")
+})
 
 
-const callButton = document.querySelector(".price.open-popup")
-const callPopup = document.querySelector(".second hide")
 
 
-const callButton = document.querySelector(".orange-inguiry.open-popup")
-const callPopup = document.querySelector(".orange hide")
+
+const inguiryButton = document.querySelector(".price")
+const inguiryPopup = document.querySelector(".second hide")
+const inguiryPopupCloseButton = inguiryPopup.querySelector(".close-feedback-black") 
+
+inguiryButton.addEventListener("click", function(){
+    inguiryPopup.classList.remove("hide")
+})
+
+inguiryPopupCloseButton.addEventListener("click", function(){
+    inguiryPopup.classList.add("hide")
+})
+
+
+
+
+
+
+const submitButton = document.querySelector(".orange-inguiry")
+const submitPopup = document.querySelector(".orange hide")
+const submitPopupCloseButton = submitPopup.querySelector(".close-feedback-white") 
+
+
+submitButton.addEventListener("click", function(){
+    submitPopup.classList.remove("hide")
+})
+
+submitPopupCloseButton.addEventListener("click", function(){
+    submitPopup.classList.add("hide")
+})
+
